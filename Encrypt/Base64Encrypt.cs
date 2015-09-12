956774120e4f00084d43f83a21f0fb4edcacc965
moonrailgun <moonrailgun@gmail.com>
2015-09-12 15:19:10 +0800

    加密解密
    ADD 加密解密工具
    ADD Base64加密解密
    ADD MD5加密

---
+++ Encrypt/Base64Encrypt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+
+namespace Encrypt
+{
+    static class Base64Encrypt
+    {
+        /// <summary>
+        /// 将文本转化为Base64编码格式
+        /// </summary>
+        public static string Encrypt(string text)
+        {
+            byte[] bytes = Encoding.Default.GetBytes(text);
+            string base64Str = Convert.ToBase64String(bytes);
+            return base64Str;
+        }
+        /// <summary>
+        /// 将图片转化为Base64编码格式
+        /// </summary>
+        public static string EncryptPic(Bitmap pic, ImageFormat imageType)
+        {
+            MemoryStream m = new MemoryStream();
+            pic.Save(m, imageType);
+            byte[] b = m.GetBuffer();
+            string base64string = Convert.ToBase64String(b);
+            return base64string;
+        }
+
+
+        /// <summary>
+        /// 将base64编码转化为文本
+        /// </summary>
+        public static string Decrypt(string base64string)
+        {
+            byte[] outputb = Convert.FromBase64String(base64string);
+            string orgStr = Encoding.Default.GetString(outputb);
+            return orgStr;
+        }
+        /// <summary>
+        /// 将base64编码转化为图片
+        /// </summary>
+        public static Bitmap DecryptPic(string base64string)
+        {
+            byte[] bt = Convert.FromBase64String(base64string);
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(bt);
+            Bitmap bitmap = new Bitmap(stream);
+            return bitmap;
+        }
+    }
+}
---
+++ Encrypt/MD5Encrypt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypt
+{
+    static class MD5Encrypt
+    {
+        public static string Encrypt(byte[] bytes, bool includeSplit = true)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] output = md5.ComputeHash(bytes);
+            return BitConverter.ToString(output);
+        }
+
+        public static string Encrypt(string text, bool includeSplit = true)
+        {
+            byte[] result = Encoding.Default.GetBytes(text);
+            string md5Str = Encrypt(result, includeSplit);
+            return md5Str;
+        }
+    }
+}
